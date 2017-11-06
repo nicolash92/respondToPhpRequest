f919076d02532664e7a3f1260eaac9fd78c3c327
nicolash92 <nicolas.hanout@gmail.com>
2017-11-06 11:47:39 -0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+/*
+var bodyParser = require('body-parser');
+var path = require ('path'); //to work with separtors on any OS including Windows
+var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
+*/
+/* GET home page. */
+router.post('/readNameAndRespond', function(req, res, next) {
+    //expecting data variable called name --retrieve value using body-parser
+    var value_name = req.query.name;  //retrieve the data associated with name
+    res.send("hello " + value_name);
+});
+
+module.exports = router;
