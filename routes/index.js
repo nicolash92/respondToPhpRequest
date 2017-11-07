cbdc3db2cb6545b8bb2c9389971a5e372369da09
nicolash92 <nicolas.hanout@gmail.com>
2017-11-06 17:31:45 -0800

    c

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var querystring = require('querystring'); //for use in GET Query string of form 
 /* GET home page. */
 router.post('/readNameAndRespond', function(req, res, next) {
     //expecting data variable called name --retrieve value using body-parser
-    var value_name = req.query.name;  //retrieve the data associated with name
+    var value_name = req.body.name;  //retrieve the data associated with name
     res.send("hello " + value_name);
 });
 
